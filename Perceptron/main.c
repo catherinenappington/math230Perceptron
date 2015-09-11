299c98ab5c64b8f833cef3fab7752e398d2ac0db
catherinenappington <catherinenappington@openmailbox.org>
2015-09-10 21:57:56 -0700

    Done
    It works as required by the assignment, but it could use some more
    modularity. As such, it will be forked.

--- Perceptron/main.c
+++ Perceptron/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define threshold 0.5
+#define learning_rate 0.1
+
 double dot_product(int x[], double y[]);
 
 int main()
 {
-    int i, j, k, network, error;
-    double threshold = 0.5, learning_rate = 0.1, sum;
+    int i, j, k, network, error, error_count = 1;
+    double sum;
     double weights[] = {0, 0, 0};
     int training_set[4][3] = { {1, 0, 0},
                           {1, 0, 1},
@@ -14,21 +17,16 @@ int main()
                           {1, 1, 1}
                           };
     int desired_output[] = {1, 1, 1, 0};
-    int error_count = 1;
 
     for(i = 0;error_count != 0; i++)
     {
+        printf(" Iteration:  %d  ************************************************************\n", i);
         error_count = 0;
-        printf("Iteration %d ***************\n", i);
-        printf("%.1f %.1f %.1f\n", weights[0], weights[1], weights[2]);
         for(j = 0; j < 4; j++)
         {
             sum = dot_product(training_set[j], weights);
 
-            if(sum > threshold)
-                network = 1;
-            else
-                network = 0;
+            network = sum > threshold ? 1 : 0;
 
             error = desired_output[j] - network;
 
@@ -39,8 +37,8 @@ int main()
             {
                 weights[k] += learning_rate * error * training_set[j][k];
             }
+            printf("%.1f %.1f %.1f\t Correction = %.5f\n", weights[0], weights[1], weights[2], error * learning_rate);
         }
-
     }
     return 0;
 }
